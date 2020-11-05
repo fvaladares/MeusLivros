cbb8657d4195c0250642d83ad1e170489be0d9fe
Fabricio Valadares <fabricio.valadares@gmail.com>
2020-11-05 10:32:58 -0300

    New books inserted.

--- app/src/main/java/br/com/pitagoras/meuslivros/MemoryRepository.kt
+++ app/src/main/java/br/com/pitagoras/meuslivros/MemoryRepository.kt
@@ -20,6 +20,25 @@ object MemoryRepository : BookRepository {
         save(Book(0, "Flutter na prática", author = "Frank Zammetti", 4.5f))
         save(Book(0, "Intermediate language practice", author = "Michael Vince", 4.5f))
         save(Book(0, "Algorithms: Theory and Practice", author = "Thomas H. Cormen et. al.", 5.0f))
+        save(Book(0, "O Conto de Aia", author = "Margaret Atwood", 5.0f))
+        save(Book(0, " O Estrangeiro", author = "Albert Camus", 5.0f))
+        save(Book(0, " O Mundo se Despedaça", author = "Chinua Achebe", 5.0f))
+        save(Book(0, " O Amor nos Tempos do Cólera", author = "Gabriel García Márquez", 5.0f))
+        save(Book(0, " Vidas secas", author = "Graciliano Ramos", 5.0f))
+        save(Book(0, " Grande Sertão Veredas", author = "Guimarães Rosa", 5.0f))
+        save(Book(0, " O Massacre da Candelária", author = "Geraldo Lopes", 5.0f))
+        save(
+            Book(
+                0,
+                " Sapiens Uma breve história da humanidade ",
+                author = "Yuval Noah Harari",
+                5.0f
+            )
+        )
+        save(Book(0, " A máquina do ódio ", author = "Patrícia Campos Mello", 5.0f))
+        save(Book(0, "O Grande Gatsby", author = "F. Scott Fitzgerald.", 5.0f))
+
+
     }
 
 
